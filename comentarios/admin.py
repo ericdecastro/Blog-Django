a838ca904c923cf795889d5c09fdcdb6662b0bf5
Eric de Castro <ericdecastro88@hotmail.com>
2021-01-13 16:10:19 -0300

    models corrigidos, migraçôes feitas e superuser criado

--- comentarios/admin.py
+++ comentarios/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from.models import Comentario
 
 
 class ComentarioAdmin(admin.ModelAdmin):
@@ -6,3 +7,6 @@ class ComentarioAdmin(admin.ModelAdmin):
                     'data_comentario', 'publicado_comentario',)
     list_editable = ('publicado_comentario',)
     list_display_links = ('id', 'nome_comentario', 'email_comentario',)
+
+
+admin.site.register(Comentario, ComentarioAdmin)
