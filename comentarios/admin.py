7f9ef7ddcfe8a6a5c834c42398cd2e3b1be8a5a1
Eric de Castro <ericdecastro88@hotmail.com>
2021-01-13 15:58:33 -0300

    criados models

--- comentarios/admin.py
+++ comentarios/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome_comentario', 'email_comentario', 'post_comentario',
+                    'data_comentario', 'publicado_comentario',)
+    list_editable = ('publicado_comentario',)
+    list_display_links = ('id', 'nome_comentario', 'email_comentario',)
