413a29852b3c7c55f00d668f0bd551f2a7b8180c
Eric de Castro <ericdecastro88@hotmail.com>
2021-01-15 15:44:30 -0300

    adicionados mais detalhes aos models e summernote configurado

--- comentarios/models.py
+++ comentarios/models.py
@@ -5,9 +5,12 @@ from django.utils import timezone
 
 
 class Comentario(models.Model):
-    nome_comentario = models.CharField(max_length=150)
-    email_comentario = models.EmailField()
-    post_comentario = models.ForeignKey(Post, on_delete=models.CASCADE)
-    usuario_comentario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    data_comentario = models.DateTimeField(default=timezone.now)
-    publicado_comentario = models.BooleanField(default=False)
+    nome_comentario = models.CharField(max_length=150, verbose_name='Nome')
+    email_comentario = models.EmailField(verbose_name='Email')
+    post_comentario = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Post')
+    usuario_comentario = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name='Usuário')
+    data_comentario = models.DateTimeField(default=timezone.now, verbose_name='Data')
+    publicado_comentario = models.BooleanField(default=False, verbose_name='Publicado')
+
+    def __str__(self):
+        return self.nome_comentario
