45e7e7fe21bd5678d0393c341163e95ac3ed3b9f
Eric de Castro <ericdecastro88@hotmail.com>
2021-03-27 20:57:59 -0300

    ajustes para deploy

--- BlogDjango/settings.py
+++ BlogDjango/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
