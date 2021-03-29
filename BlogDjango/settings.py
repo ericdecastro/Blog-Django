97920a0614f178888cd9c856b9acb0e44b75633e
Eric de Castro <ericdecastro88@hotmail.com>
2021-03-29 14:55:52 -0300

    amazon s3

--- BlogDjango/settings.py
+++ BlogDjango/settings.py
@@ -168,3 +168,11 @@ LOGIN_URL = 'entrar'
 LOGIN_REDIRECT_URL = 'index'
 
 django_heroku.settings(locals())
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'django-blog-ericdecastro'
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
