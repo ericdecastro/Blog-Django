94b1488723be1e9e46c96af0c1c849ee61f2a2d9
Eric de Castro <ericdecastro88@hotmail.com>
2021-03-29 17:35:53 -0300

    alterações de chave

--- BlogDjango/settings.py
+++ BlogDjango/settings.py
@@ -170,22 +170,13 @@ LOGIN_URL = 'entrar'
 LOGIN_REDIRECT_URL = 'index'
 
 django_heroku.settings(locals())
-
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'django-blog-ericdecastro2'
-AWS_S3_SIGNATURE_VERSION = "s3v4"
-
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# from botocore.client import Config
 #
+# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = 'django-blog-ericdecastro2'
+# AWS_S3_SIGNATURE_VERSION = "s3v4"
 #
-# s3 = boto3.resource(
-#     's3',
-#     aws_access_key_id=config('AWS_ACCESS_KEY_ID'),
-#     aws_secret_access_key=config('AWS_SECRET_ACCESS_KEY'),
-#     config=Config(signature_version='s3v4')
-# )
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
