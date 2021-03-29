6090c0307edb590cae848f4d00f1f58951d0a146
Eric de Castro <ericdecastro88@hotmail.com>
2021-03-29 15:39:47 -0300

    amazon s3

--- BlogDjango/settings.py
+++ BlogDjango/settings.py
@@ -174,7 +174,7 @@ django_heroku.settings(locals())
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'django-blog-ericdecastro'
-AWS_S3_REGION_NAME = "ap-south-1"
+AWS_S3_REGION_NAME = "us-east-2"
 AWS_S3_SIGNATURE_VERSION = "s3v4"
 
 AWS_S3_FILE_OVERWRITE = False
