b2afdc4acdbc3ba7ba9a35050da7ada4ac73cec8
Eric de Castro <ericdecastro88@hotmail.com>
2021-03-31 15:58:59 -0300

    gitignore

--- posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', PostIndex.as_view(), name='index'),
-    path('categoria/<str:categoria>', PostCategoria.as_view(), name='post_categoria'),
-    path('busca/', PostBusca.as_view(), name='post_busca'),
-    path('post/<int:pk>', PostDetalhes.as_view(), name='post_detalhes'),
-]
